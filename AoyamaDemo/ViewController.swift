86fcb3ac456e2e9fe04203aea4c77bedeca170ed
Dongri Jin <dongriat@gmail.com>
2015-05-31 23:40:35 +0900

    🍺🍺🍺

--- AoyamaDemo/ViewController.swift
+++ AoyamaDemo/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var logView: UITextView!
+    
+    let aoyama = Aoyama.sharedInstance
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //aoyama.baseURL = "https://resters.herokuapp.com"
+        aoyama.baseURL = "http://localhost:5000"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func getRequest(sender: AnyObject) {
+        aoyama.request(.GET, path: "/users") { (res, data, error) -> Void in
+            var dict = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSArray
+            self.logView.text = "\(dict)"
+        }
+    }
+    
+    @IBAction func postRequest(sender: AnyObject) {
+        let parameters = ["name": "YuanJin",]
+        aoyama.request(.POST, path:"/users", parameters: parameters) { (res, data, error) -> Void in
+            var dict = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSArray
+            self.logView.text = "\(dict)"
+        }
+    }
+    
+    
+    @IBAction func putRequest(sender: AnyObject) {
+        let parameters = ["name": "Apple",]
+        aoyama.request(.PUT, path: "/users/1", parameters: parameters) { (res, data, error) -> Void in
+            var dict = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+            self.logView.text = "\(dict)"
+        }
+    }
+    
+    @IBAction func deleteRequest(sender: AnyObject) {
+        aoyama.request(.DELETE, path: "/users/1") { (res, data, error) -> Void in
+            var dict = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+            self.logView.text = "\(dict)"
+        }
+    }
 }
 
